b5df467c77b7d4f28d934710f45a6fb5b59f52a9
Pankaj Kumar <pankaj3359kr@gmail.com>
2020-01-08 15:40:38 +0530

    Updates

---
+++ precache-manifest.7ab3914fab34b69ab56f610fe4d886db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc52e27d4a664a9a1d0a65ad500a5020",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "e317bc990c8fe2a1560a",
+    "url": "/react-portfolio/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "8d31de8e377030275d1f",
+    "url": "/react-portfolio/static/js/2.e37d76c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-portfolio/static/js/2.e37d76c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e317bc990c8fe2a1560a",
+    "url": "/react-portfolio/static/js/main.c0fcdb95.chunk.js"
+  },
+  {
+    "revision": "dbf3362b198c29308777",
+    "url": "/react-portfolio/static/js/runtime-main.5c1014ea.js"
+  },
+  {
+    "revision": "babfe3a4b064896311914c9d39c713bc",
+    "url": "/react-portfolio/static/media/profile.babfe3a4.jfif"
+  }
+]);
